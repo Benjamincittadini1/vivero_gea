aaa286df168b313f09f416e2f65d7220c5e6ad4d
Benjamincittadini1 <86275653+Benjamincittadini1@users.noreply.github.com>
2021-10-19 19:42:50 -0300

    Adding NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+const NavBar = () => {
+    return(
+        <div className="menu">
+    <div className="title">VIVERO GEA</div>
+    <ul className="nav">
+      <li><a href="../public/index.html">Inicio</a></li>
+      <li><a href="../public/Catálogo.html">Catálogo</a></li>
+      <li><a href="../public/Nosotros.html">Nosotros</a></li>
+    </ul>
+    </div>
+    )
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-
 import ReactDOM from "react-dom"
+import App from "./App"
+import "./styles.css"
+import "./media/fondo.jpg"
 
-const Elemento = () => <p>Hola Mundo jojo</p>
-
-ReactDOM.render( <Elemento/> , document.getElementById("root"))
+ReactDOM.render( <App/> , document.getElementById("root"))
